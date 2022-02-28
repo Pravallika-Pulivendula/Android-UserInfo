8c9b1d02c114f956664be1e054c7c0f04dc1961e
Pravallika-Pulivendula <pravallika.p@everest.engineering>
2022-02-28 18:35:47 +0530

    Add confirm feature

--- app/src/main/java/com/everest/userinfo/ConfirmActivity.kt
+++ app/src/main/java/com/everest/userinfo/ConfirmActivity.kt
@@ -1,4 +1,29 @@
 package com.everest.userinfo
 
-class ConfirmActivity {
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.everest.userinfo.databinding.ActivityConfirmBinding
+
+class ConfirmActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityConfirmBinding
+    private lateinit var detailsTV: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityConfirmBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        detailsTV = binding.detailsTV
+        val userName = intent.getStringExtra(USERNAME)
+        val email = intent.getStringExtra(EMAIL)
+        val phoneNumber = intent.getStringExtra(PHONE_NUMBER)
+        val pincode = intent.getStringExtra(PINCODE)
+        val address = intent.getStringExtra(ADDRESS)
+
+        val resultText =
+            "Hi $userName, How are you? Are you staying at $address-$pincode. I am not able to contact you on $phoneNumber. Can I email you the details at $email"
+
+        detailsTV.text = resultText
+    }
 }
