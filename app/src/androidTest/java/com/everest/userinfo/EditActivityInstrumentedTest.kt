f04b6a1b2586fb89ad4c227810fe50f4f77d6888
Pravallika-Pulivendula <pravallika.p@everest.engineering>
2022-03-02 00:37:31 +0530

    Add instrumentation test for validation

--- app/src/androidTest/java/com/everest/userinfo/EditActivityInstrumentedTest.kt
+++ app/src/androidTest/java/com/everest/userinfo/EditActivityInstrumentedTest.kt
@@ -1,4 +1,44 @@
 package com.everest.userinfo
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
 class EditActivityInstrumentedTest {
+    @get:Rule
+    val activityScenario = ActivityScenarioRule(EditActivity::class.java)
+
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        Assert.assertEquals("com.everest.userinfo", appContext.packageName)
+    }
+
+    @Test
+    fun shouldValidateData() {
+        onView(withId(R.id.usernameET)).perform(typeText("Pravallika"), closeSoftKeyboard())
+        onView(withId(R.id.emailET)).perform(typeText("pravallika@gmail.com"), closeSoftKeyboard())
+        onView(withId(R.id.phoneNumberET)).perform(typeText("9189934567"), closeSoftKeyboard())
+        onView(withId(R.id.pincodeET)).perform(typeText("890765"), closeSoftKeyboard())
+        onView(withId(R.id.addressET)).perform(typeText("Pulivendula"), closeSoftKeyboard())
+
+        onView(withId(R.id.validateButton)).perform(click())
+
+        onView(withId(R.id.usernameET)).check(matches(withText("Pravallika")))
+        onView(withId(R.id.emailET)).check(matches(withText("pravallika@gmail.com")))
+        onView(withId(R.id.phoneNumberET)).check(matches(withText("9189934567")))
+        onView(withId(R.id.pincodeET)).check(matches(withText("890765")))
+        onView(withId(R.id.addressET)).check(matches(withText("Pulivendula")))
+
+    }
 }
