87243f17cd2079cfe94133c64f89071ca92184d8
Pravallika-Pulivendula <pravallika.p@everest.engineering>
2022-03-02 16:21:23 +0530

    Add instrumented tests for confirm and cancel buttons

--- app/src/androidTest/java/com/everest/userinfo/EditActivityInstrumentedTest.kt
+++ app/src/androidTest/java/com/everest/userinfo/EditActivityInstrumentedTest.kt
@@ -1,7 +1,5 @@
 package com.everest.userinfo
 
-import android.text.Html
-import androidx.core.text.HtmlCompat
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
@@ -27,7 +25,7 @@ class EditActivityInstrumentedTest {
     }
 
     @Test
-    fun shouldValidateData() {
+    fun shouldValidateDataAndAllowUserToEditEnteredDataWhenCancelButtonIsClicked() {
         onView(withId(R.id.usernameET)).perform(typeText("Pravallika"), closeSoftKeyboard())
         onView(withId(R.id.emailET)).perform(typeText("pravallika@gmail.com"), closeSoftKeyboard())
         onView(withId(R.id.phoneNumberET)).perform(typeText("9189934567"), closeSoftKeyboard())
@@ -35,6 +33,44 @@ class EditActivityInstrumentedTest {
         onView(withId(R.id.addressET)).perform(typeText("Pulivendula"), closeSoftKeyboard())
 
         onView(withId(R.id.validateButton)).perform(click())
+
+        onView(withId(R.id.usernameET)).check(matches(isNotEnabled()))
+        onView(withId(R.id.emailET)).check(matches(isNotEnabled()))
+        onView(withId(R.id.phoneNumberET)).check(matches(isNotEnabled()))
+        onView(withId(R.id.pincodeET)).check(matches(isNotEnabled()))
+        onView(withId(R.id.addressET)).check(matches(isNotEnabled()))
+
+        onView(withId(R.id.usernameET)).check(matches(withText("Pravallika")))
+        onView(withId(R.id.emailET)).check(matches(withText("pravallika@gmail.com")))
+        onView(withId(R.id.phoneNumberET)).check(matches(withText("9189934567")))
+        onView(withId(R.id.pincodeET)).check(matches(withText("890765")))
+        onView(withId(R.id.addressET)).check(matches(withText("Pulivendula")))
+
+        onView(withId(R.id.cancelButton)).perform(click())
+
+        onView(withId(R.id.usernameET)).check(matches(isEnabled()))
+        onView(withId(R.id.emailET)).check(matches(isEnabled()))
+        onView(withId(R.id.phoneNumberET)).check(matches(isEnabled()))
+        onView(withId(R.id.pincodeET)).check(matches(isEnabled()))
+        onView(withId(R.id.addressET)).check(matches(isEnabled()))
+
+    }
+
+    @Test
+    fun shouldValidateDataAndDisplayTheUserSummaryWhenConfirmButtonIsClicked() {
+        onView(withId(R.id.usernameET)).perform(typeText("Pravallika"), closeSoftKeyboard())
+        onView(withId(R.id.emailET)).perform(typeText("pravallika@gmail.com"), closeSoftKeyboard())
+        onView(withId(R.id.phoneNumberET)).perform(typeText("9189934567"), closeSoftKeyboard())
+        onView(withId(R.id.pincodeET)).perform(typeText("890765"), closeSoftKeyboard())
+        onView(withId(R.id.addressET)).perform(typeText("Pulivendula"), closeSoftKeyboard())
+
+        onView(withId(R.id.validateButton)).perform(click())
+
+        onView(withId(R.id.usernameET)).check(matches(isNotEnabled()))
+        onView(withId(R.id.emailET)).check(matches(isNotEnabled()))
+        onView(withId(R.id.phoneNumberET)).check(matches(isNotEnabled()))
+        onView(withId(R.id.pincodeET)).check(matches(isNotEnabled()))
+        onView(withId(R.id.addressET)).check(matches(isNotEnabled()))
 
         onView(withId(R.id.usernameET)).check(matches(withText("Pravallika")))
         onView(withId(R.id.emailET)).check(matches(withText("pravallika@gmail.com")))
@@ -44,9 +80,9 @@ class EditActivityInstrumentedTest {
 
         onView(withId(R.id.confirmButton)).perform(click())
 
-        val resultText = "Hi Pravallika, How are you? Are you staying at Pulivendula-890765. I am not able to contact you on 9189934567. Can I email you the details at pravallika@gmail.com"
+        val resultText =
+            "Hi Pravallika, How are you? Are you staying at Pulivendula-890765. I am not able to contact you on 9189934567. Can I email you the details at pravallika@gmail.com"
 
         onView(withId(R.id.detailsTV)).check(matches(withText(resultText)))
-
     }
 }
