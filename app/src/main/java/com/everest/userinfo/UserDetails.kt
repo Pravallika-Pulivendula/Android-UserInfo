bb475c78bc4715d4f772aa7c3b9db5c48b4af4a5
Pravallika-Pulivendula <pravallika.p@everest.engineering>
2022-03-06 10:23:29 +0530

    Add data class

--- app/src/main/java/com/everest/userinfo/UserDetails.kt
+++ app/src/main/java/com/everest/userinfo/UserDetails.kt
@@ -1,4 +1,15 @@
 package com.everest.userinfo
 
-class UserDetails {
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+
+@Parcelize
+class UserDetails(
+    var userName: String,
+    var email: String,
+    var phoneNumber: String,
+    var pincode: String,
+    var address: String
+) : Parcelable {
 }
