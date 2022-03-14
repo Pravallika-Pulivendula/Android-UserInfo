7c7055d534cf1748daf80dae4e363ea6c1edd1d6
Pravallika-Pulivendula <pravallika.p@everest.engineering>
2022-03-15 00:19:52 +0530

    Add fragments

--- app/src/main/java/com/everest/userinfo/HomeActivity.kt
+++ app/src/main/java/com/everest/userinfo/HomeActivity.kt
@@ -1,4 +1,32 @@
 package com.everest.userinfo
 
-class HomeActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.everest.userinfo.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        super.onCreate(savedInstanceState)
+
+        val editFragment = EditFragment()
+
+        if (savedInstanceState == null) {
+            loadFragment(editFragment)
+        }
+
+    }
+
+    private fun loadFragment(editFragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_one, editFragment)
+            commit()
+        }
+    }
+
 }
