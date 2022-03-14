d425d4eb54208c3d29c27016eb6e5efef69fd3d1
Pravallika-Pulivendula <pravallika.p@everest.engineering>
2022-03-15 00:17:20 +0530

    Add fragment to edit details

--- app/src/main/java/com/everest/userinfo/EditFragment.kt
+++ app/src/main/java/com/everest/userinfo/EditFragment.kt
@@ -1,59 +1,89 @@
 package com.everest.userinfo
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.everest.userinfo.databinding.FragmentEditBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [EditFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class EditFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private var _binding: FragmentEditBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var validator: Validator
+    private lateinit var editViewModel: EditViewModel
+    private lateinit var displayFragment: DisplayFragment
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        if (_binding == null) {
+            _binding = FragmentEditBinding.inflate(inflater, container, false)
+        }
+        validator = Validator()
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        editViewModel = activity?.let { ViewModelProvider(it)[EditViewModel::class.java] }
+            ?: throw RuntimeException("Not a Activity")
+        displayFragment = DisplayFragment()
+
+        binding.validateButton.setOnClickListener {
+            validateData()
+        }
+
+        binding.cancelButton.setOnClickListener {
+            editViewModel.setVisibility(false)
+            modifyViews(editViewModel.visibility.value)
+        }
+
+        binding.confirmButton.setOnClickListener {
+            editViewModel.setUserData(
+                binding.usernameET.text.toString(),
+                binding.emailET.text.toString(),
+                binding.phoneNumberET.text.toString(),
+                binding.pincodeET.text.toString(),
+                binding.addressET.text.toString()
+            )
+
+            replaceFragment(displayFragment)
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit, container, false)
+    private fun validateData() {
+        if (validator.areFieldsEmpty(
+                binding.layout,
+                requireActivity()
+            ) && validator.isEmailValid(
+                binding.emailET.text.toString(), requireActivity()
+            ) && validator.isPhoneNumberValid(
+                binding.phoneNumberET.text.toString(), requireActivity()
+            ) && validator.isPinCodeValid(binding.pincodeET.text.toString(), requireActivity())
+        ) {
+            editViewModel.isDataValid(true)
+            editViewModel.setVisibility(true)
+            modifyViews(editViewModel.visibility.value)
+        }
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment EditFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            EditFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun modifyViews(visibility: Boolean?) {
+        editViewModel.changeVisibility(binding.buttonsVisibility, binding.validateButton)
+        editViewModel.customizeEditText(binding.layout, !visibility!!)
     }
+
+    private fun replaceFragment(displayFragment: DisplayFragment) {
+        activity?.supportFragmentManager?.beginTransaction()?.apply {
+            replace(R.id.fragment_one, displayFragment)
+            addToBackStack(null)
+            commit()
+        }
+    }
+
 }
